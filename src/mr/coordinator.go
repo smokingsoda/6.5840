54ed96b86a4cf2a57a65c07a186989000b91ca53
Junru Wang <junru.wang.24@ucl.ac.uk>
2025-06-25 11:14:48 +0100

    Implement task allocation functions both for worker and coordinator.

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,33 +1,110 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"time"
+)
 
+type Task struct {
+	fileName         string
+	lastResponseTime time.Time
+	status           bool
+	id               int
+}
 
 type Coordinator struct {
 	// Your definitions here.
+	tasks   []Task
+	files   []string
+	nMap    int
+	mReduce int
 
+	heartbeatCh chan HeartbeatMsg
+	reportCh    chan ReportMsg
+	doneCh      chan struct{}
+
+	// Lock-free file allocation channels
+	fileRequestCh  chan chan string // Channel for worker file requests
+	availableFiles chan string      // Channel for available files
+}
+
+type HeartbeatMsg struct {
+	// Heartbeat message
+	response *HeartbeatResponse
+	ok       chan struct{}
+}
+
+type ReportMsg struct {
+	request *ReportRequest
+	ok      chan struct{}
+}
+
+// Coordinator's response after receiving heartbeat from Worker
+type HeartbeatResponse struct {
+}
+
+// Worker's request to report task completion status to Coordinator
+type ReportRequest struct {
+}
+
+// File allocator - implements lock-free using channels
+func (c *Coordinator) fileAllocator() {
+	for {
+		select {
+		case responseCh := <-c.fileRequestCh:
+			// Received file request from worker
+			select {
+			case fileName := <-c.availableFiles:
+				// Available file found, assign to worker
+				responseCh <- fileName
+			default:
+				// No available files, return empty string to indicate completion
+				responseCh <- ""
+			}
+		case <-c.doneCh:
+			return
+		}
+	}
 }
 
 // Your code here -- RPC handlers for the worker to call.
+func (c *Coordinator) RequestTask(args *RequestTaskArgs, reply *RequestTaskReply) error {
+	// Create response channel
+	responseCh := make(chan string)
 
-//
+	// Request file from allocator
+	c.fileRequestCh <- responseCh
+
+	// Wait for file allocation result
+	fileName := <-responseCh
+
+	if fileName != "" {
+		reply.TaskType = "map"
+		reply.FileName = fileName
+		log.Printf("Assigned file %s to worker", fileName)
+	} else {
+		reply.TaskType = "done"
+		reply.FileName = ""
+		log.Printf("All files have been assigned")
+		c.doneCh <- struct{}{}
+	}
+
+	return nil
+}
+
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
-
-//
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,29 +118,36 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
 	ret := false
 
 	// Your code here.
 
-
 	return ret
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
 
-	// Your code here.
+	// Initialize channels
+	c.fileRequestCh = make(chan chan string)
+	c.availableFiles = make(chan string, len(files))
+	c.doneCh = make(chan struct{})
 
+	// Put all files into available files channel
+	for _, file := range files {
+		c.availableFiles <- file
+	}
+
+	log.Printf("Coordinator started with %d files to assign", len(files))
+
+	// Start file allocator goroutine
+	go c.fileAllocator()
 
 	c.server()
 	return &c
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,48 +1,69 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
+import (
+	"fmt"
+	"hash/fnv"
+	"log"
+	"net/rpc"
+)
 
-
-//
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
+type RequestTaskArgs struct{}
+
+type RequestTaskReply struct {
+	TaskType string
+	FileName string
+}
+
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
-//
 // main/mrworker.go calls this function.
-//
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
 	// Your worker implementation here.
+	fmt.Printf("Worker started, requesting tasks...\n")
+
+	// for {
+	args := RequestTaskArgs{}
+	reply := RequestTaskReply{}
+	ok := call("Coordinator.RequestTask", &args, &reply)
+
+	if !ok {
+		fmt.Printf("Cannot connect to coordinator, exiting\n")
+		return
+	}
+
+	if reply.TaskType == "done" {
+		fmt.Printf("All tasks completed, worker exiting\n")
+		return
+	}
+
+	if reply.TaskType == "map" && reply.FileName != "" {
+		fmt.Printf("🎉 Received task! Filename: %s\n", reply.FileName)
+		// Add actual map task processing logic here
+		// Currently just printing the filename
+	}
+	// }
 
 	// uncomment to send the Example RPC to the coordinator.
 	// CallExample()
-
 }
 
-//
 // example function to show how to make an RPC call to the coordinator.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func CallExample() {
 
 	// declare an argument structure.
@@ -67,11 +88,9 @@ func CallExample() {
 	}
 }
 
-//
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
